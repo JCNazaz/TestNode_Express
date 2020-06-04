b7b15e788fe95e426c38d4a30579bcb5f511012a
Fabiano Araujo <63600082+JCNazaz@users.noreply.github.com>
2020-06-04 20:52:20 +0100

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+const port = 3000
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+app.post('/', function(req, res){
+let num1 = Number(req.body.num1);
+let num2 = Number(req.body.num1);
+let result = num1 + num2;
+
+res.send(`The result of the calculation is ${result}`);
+
+});
+app.get('/bmicalculator', function(req, res){
+    res.sendFile(__dirname + '/bmiCalculator.html');
+  });
+app.post('/bmicalculator', function(req, res){
+  let weight = parseFloat(req.body.weight);
+  let height = parseFloat(req.body.height);
+  let result = (weight / Math.pow(height,2));
+
+  res.send(`Your BMI is ${result}`);
+})
+
+
+  app.get('/bmicalculator', function(req, res){
+    res.sendFile(__dirname + '/bmiCalculator.html');
+  });
+app.post('/bmicalculator', function(req, res){
+  let weight = parseFloat(req.body.weight);
+  let height = parseFloat(req.body.height);
+  let result = (weight / Math.pow(height,2));
+
+  res.send(`Your BMI is ${result}`);
+})
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
